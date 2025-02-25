85b03a060a9f408dc9cb5c324f77f1eec0aaef22
Adil Iqbal Iqbal <ai9069935@gmail.com>
2025-02-25 14:39:21 +0100

    # Módulo de preparación de pizzas

--- pizzeriamodular.py
+++ pizzeriamodular.py
@@ -12,3 +12,19 @@ class Pedido:
     def completar_pedido(self):
         self.estado = "Completado"
         print(f"Pedido para {self.cliente} completado!")
+
+    # Módulo de preparación de pizzas
+class Pizza:
+    def __init__(self, nombre, ingredientes):
+        self.nombre = nombre
+        self.ingredientes = ingredientes
+    
+    def preparar(self, inventario):
+        for ingrediente in self.ingredientes:
+            if inventario.verificar_ingrediente(ingrediente):
+                inventario.utilizar_ingrediente(ingrediente)
+            else:
+                print(f"Falta {ingrediente}, no se puede preparar la pizza {self.nombre}")
+                return False
+        print(f"Pizza {self.nombre} preparada!")
+        return True
